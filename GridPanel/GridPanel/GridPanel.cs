b9e67f36e2119fd63f79bbd2cb4af09c568cbfe3
Sam <sblanchard@synergiz.com>
2019-02-20 20:29:11 +0100

    First commit

---
+++ GridPanel/GridPanel/GridPanel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace GridPanels
+{
+    public class GridPanel : Grid
+    {
+        public GridPanel() : base()
+        {
+        }
+
+        protected override void OnIsItemsHostChanged(bool oldIsItemsHost, bool newIsItemsHost)
+        {
+            base.OnIsItemsHostChanged(oldIsItemsHost, newIsItemsHost);
+
+            if(newIsItemsHost == true)
+            {
+                this.ColumnDefinitions.Clear();
+                this.RowDefinitions.Clear();
+            }
+        }
+
+        protected override void OnVisualChildrenChanged(DependencyObject visualAdded, DependencyObject visualRemoved)
+        {
+            base.OnVisualChildrenChanged(visualAdded, visualRemoved);
+
+            if(visualAdded != null)
+            {
+                this.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+            }
+
+            if(visualRemoved != null)
+            {
+                this.ColumnDefinitions.RemoveAt(0);
+            }
+
+            for (int i = 0; i < this.InternalChildren.Count; i++)
+            {
+                var child = this.InternalChildren[i];
+                Grid.SetColumn(child, i);
+            }
+        }
+    }
+}
---
+++ GridPanel/GridPanel/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GridPanels
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.LayoutRoot.DataContext = this;
+
+            this.Items.Add("Toto");
+            this.Items.Add("Titi");
+        }
+
+        /// <summary>
+        /// Items de type string
+        /// </summary>
+
+        public ObservableCollection<string> Items
+        {
+            get { return (ObservableCollection<string>)GetValue(ItemsProperty); }
+            set { SetValue(ItemsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Items.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ItemsProperty =
+            DependencyProperty.Register("Items", typeof(ObservableCollection<string>), typeof(MainWindow), new PropertyMetadata(new ObservableCollection<string>()));
+
+        private void ButtonAddItems_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Items != null)
+            {
+                this.Items.Add("Tutu" + this.Items.Count);
+            }
+        }
+    }
+}
