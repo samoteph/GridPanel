2616f613c13eb77433998d962dc891b9694ffa81
Sam <sblanchard@synergiz.com>
2019-02-20 21:21:34 +0100

    Add Orientation

--- GridPanel/GridPanel/GridPanel.cs
+++ GridPanel/GridPanel/GridPanel.cs
@@ -6,12 +6,69 @@ using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows;
 
-namespace GridPanels
+namespace SamSoft.Windows
 {
     public class GridPanel : Grid
     {
         public GridPanel() : base()
         {
+        }
+
+        /// <summary>
+        /// Orientation
+        /// </summary>
+
+        public Orientation Orientation
+        {
+            get { return (Orientation)GetValue(OrientationProperty); }
+            set { SetValue(OrientationProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Orientation.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty OrientationProperty =
+            DependencyProperty.Register("Orientation", typeof(Orientation), typeof(GridPanel), new PropertyMetadata(Orientation.Horizontal, OnOrientationChange));
+
+        private static void OnOrientationChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var gridPanel = d as GridPanel;
+            gridPanel.ChangeOrientation((Orientation)e.NewValue);
+        }
+
+        private void ChangeOrientation(Orientation orientation)
+        {
+            this.ColumnDefinitions.Clear();
+            this.RowDefinitions.Clear();
+
+            for (int i = 0; i < this.InternalChildren.Count; i++)
+            {
+                this.AddGridDefinition(orientation);
+            }
+
+            this.ApplyGridIndex();
+        }
+
+        private void AddGridDefinition(Orientation orientation)
+        {
+            if (orientation == Orientation.Horizontal)
+            {
+                this.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+            }
+            else
+            {
+                this.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+            }
+        }
+
+        private void RemoveGridDefinition(Orientation orientation)
+        {
+            if (orientation == Orientation.Horizontal)
+            {
+                this.ColumnDefinitions.RemoveAt(0);
+            }
+            else
+            {
+                this.RowDefinitions.RemoveAt(0);
+            }
         }
 
         protected override void OnIsItemsHostChanged(bool oldIsItemsHost, bool newIsItemsHost)
@@ -31,18 +88,31 @@ namespace GridPanels
 
             if(visualAdded != null)
             {
-                this.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+                this.AddGridDefinition(this.Orientation);
             }
 
             if(visualRemoved != null)
             {
-                this.ColumnDefinitions.RemoveAt(0);
+                this.RemoveGridDefinition(this.Orientation);
             }
 
+            this.ApplyGridIndex();
+        }
+
+        private void ApplyGridIndex()
+        {
             for (int i = 0; i < this.InternalChildren.Count; i++)
             {
                 var child = this.InternalChildren[i];
-                Grid.SetColumn(child, i);
+
+                if (this.Orientation == Orientation.Horizontal)
+                {
+                    Grid.SetColumn(child, i);
+                }
+                else
+                {
+                    Grid.SetRow(child, i);
+                }
             }
         }
     }
